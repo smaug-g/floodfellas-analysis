2e13a838abbea3eceb2e82f2317f057ccd64012b
smaug-g <smg10@rice.edu>
2020-04-22 14:49:40 -0500

    Prettier radar plot

--- radar/localAnalysis.py
+++ radar/localAnalysis.py
@@ -15,7 +15,11 @@ with open('sensor_vals.pkl', 'rb') as f:
 date_nums = saved_data[0]
 sensor_values = np.array(saved_data[1])[:,1:]
 sensor_med = [np.median(x) for x in sensor_values]
-med_med = signal.medfilt(sensor_med, [15])
+
+interesting = sensor_med[500:]
+
+med_med = signal.medfilt(interesting, [21])
+med_med = (-med_med + np.mean(med_med))
 
 #fs = 1/300
 #bands =   [0, 1*fs/16, 2*fs/8, 3*fs/8] 
@@ -28,8 +32,11 @@ med_med = signal.medfilt(sensor_med, [15])
 
 fig = matplotlib.pyplot.figure()
 fig.suptitle("2 weeks of Radar Data", fontsize=38)
-matplotlib.pyplot.plot_date(date_nums, med_med, ls='-', ms=0)
-matplotlib.pyplot.xlabel("Time (days)", fontsize=20)
-matplotlib.pyplot.ylabel("Distance From Sensor to Target (mm)", fontsize=20)
+#matplotlib.pyplot.plot_date(date_nums, med_med, ls='-', ms=0)
+matplotlib.pyplot.plot(med_med, ls='-', ms=0)
+matplotlib.pyplot.xlabel("Time (5 min / tick)", fontsize=20)
+matplotlib.pyplot.ylabel("Mean Subtracted Target Height (mm)", fontsize=20)
+matplotlib.pyplot.minorticks_on()
+matplotlib.pyplot.grid(b=None, which='both', axis='both')
 #matplotlib.pyplot.plot_date(date_nums, sensor_values)
 matplotlib.pyplot.show()
